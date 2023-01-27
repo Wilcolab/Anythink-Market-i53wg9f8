305eb006ec5d42f04417eedd327a867dcee79c45
Ann <happy19968587@gmail.com>
2023-01-28 00:19:51 +0800

    Add a search box to the main page

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,14 +1,30 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
+  const onSearchChange = (e) => {
+    if (e.target.value.length < 3) {
+      return;
+    }
+    props.onSearchFilter(
+      e.target.value, (page) => {
+        agent.Items.byTitle(e.target.value)
+        agent.Items.byTitle(e.target.value)
+      }
+    )
+
+  }
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span>A place to </span>
-          <span id="get-part">get</span>
+          <span id="get-part">get </span>
+          <form>
+            <input id="search-box" type="text" placeholder="What is it that you truly desire?" style={{ width: "300px" }} onChange={onSearchChange}></input>
+          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
