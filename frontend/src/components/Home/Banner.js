ad1c5c264563ceaacdd0873087cf19c6efbac8bf
Ann <happy19968587@gmail.com>
2023-01-28 15:33:38 +0800

    search box appear only after a visitor clicks the word “get”

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
 const Banner = (props) => {
+  const [searchVisible, setSearchVisible] = useState(false);
   const onSearchChange = (e) => {
     if (e.target.value.length < 3) {
       return;
@@ -12,22 +13,29 @@ const Banner = (props) => {
       agent.Items.byTitle(e.target.value);
     });
   };
+  const onClick = () => {
+    setSearchVisible(true);
+  };
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span>A place to </span>
-          <span id="get-part">get </span>
-          <form>
-            <input
-              id="search-box"
-              type="text"
-              placeholder="What is it that you truly desire?"
-              style={{ width: "300px" }}
-              onChange={onSearchChange}
-            ></input>
-          </form>
+          <span id="get-part" onClick={onClick}>
+            get
+          </span>
+          {searchVisible && (
+            <form>
+              <input
+                id="search-box"
+                type="text"
+                placeholder="What is it that you truly desire?"
+                style={{ width: "300px" }}
+                onChange={onSearchChange}
+              ></input>
+            </form>
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
